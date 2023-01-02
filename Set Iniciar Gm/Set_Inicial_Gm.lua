92107a306dcddb93d1be3775fd4842db3dc3151a
Ligthsx <102430118+Ligthsx@users.noreply.github.com>
2023-01-02 18:50:44 -0400

    Add files via upload

---
+++ Set Iniciar Gm/Set_Inicial_Gm.lua
@@ -0,0 +1,19 @@
+
+local function OnPlayerCommand(event, player, command)
+    gmRank = player:GetGMRank()
+    if (gmRank > 0 and command == "setgm")then -- Comando que te dara el set de gm.
+        player:AddItem(43651)
+        player:AddItem(12064)
+        player:AddItem(2586)
+        player:AddItem(11508)
+        player:AddItem(18582)
+        player:AddItem(17)
+        player:AddItem(192)
+        player:AddItem(23162, 4)
+        return false
+    end
+        player:SendBroadcastMessage("|cffFF0000Necesitas ser Maestro de Juego para poder acceder a este comando")
+        return false
+end
+
+RegisterPlayerEvent(42, OnPlayerCommand)
